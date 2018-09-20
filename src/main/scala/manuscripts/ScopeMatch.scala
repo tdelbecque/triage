fa0e002ec34cc77fe92d786b5900715c21d520e1
tdelbecque <git@sodad.com>
2018-09-20 18:48:11 +0200

    method1

--- src/main/scala/manuscripts/ScopeMatch.scala
+++ src/main/scala/manuscripts/ScopeMatch.scala
@@ -8,6 +8,15 @@ case class EmbeddingExtRecord (
   issn: String, 
   subjareas: Seq[String])
 
+class ScopeMatch (
+  val issn: String,
+  val scores: Map[String, Double],
+  val freqHistoRef: (Long, Map[String, (Long, Double)]),
+  val freqHistoTest: (Long, Map[String, (Long, Double)]) ) 
+{
+  def value = scores.getOrElse (issn, 0.0)
+}
+
 class ScopeMatchApp (val embeddingApp : EmbeddingApp) {
   import embeddingApp._
   import session.implicits._
@@ -95,13 +104,11 @@ class ScopeMatchApp (val embeddingApp : EmbeddingApp) {
     Some (mostInScope)
   }
 
-  /** First method of Scope Match dimension extraction
-    * It is based on comparing the histograms of journals for given 
-    * subject areas, computed on the manuscripts dataset on one hand and
-    * inside a neighborhood of the submitted paper on the other hand. 
-    *
-    */
-  def method1 (subEid: String, targetIssn: String, neighborhoodSize: Int) = {
+  def method1ForEmbedding (
+    subEmbedding: Seq[Double], 
+    targetIssn: String, 
+    neighborhoodSize: Int, 
+    remEid: String = "") : ScopeMatch = {
     /* gather in a map the nb of occurrences of each issns in
      * the argument dataset */ 
     def histo (data: Dataset[_]) = data.
@@ -117,7 +124,65 @@ class ScopeMatchApp (val embeddingApp : EmbeddingApp) {
     }
 
     /* find all the papers that share at least one subject area 
-     * with the submitted paper. They are the "slibings" */
+     * with the targeted issn. They are the "slibings" */
+    val targetSubjArea : Seq[String]= subjareasMap (targetIssn).toSeq
+    val slibingsManuscripts = manuscripts.
+      filter { ! _.subjareas.intersect (targetSubjArea).isEmpty }.
+      filter { _.eid != remEid }
+    val slibingsEmbeddings = manuscriptsEmbeddings.
+      join (slibingsManuscripts, $"id" ===  $"eid").
+      select ("id", "embedding").
+      as[ManuscriptEmbeddingRecord]
+    val referenceHistogram = histo (slibingsManuscripts)
+
+    /* computes histograms in a neighborhood of the submitted paper */
+    val neighborEids = mostSimilars (Seq (subEmbedding), slibingsEmbeddings, neighborhoodSize).head
+    val neighborEidsDataframe = sc.parallelize (neighborEids).toDF ("eid", "distance")
+    val neighborManuscripts = slibingsManuscripts.join (neighborEidsDataframe, "eid")
+    val neighborHistogram = histo (neighborManuscripts)
+
+    val (nRef, freqHistoRef) = freqHisto (referenceHistogram)
+    val (nTest, freqHistoTest) = freqHisto (neighborHistogram)
+
+    val scores = freqHistoTest.map { case (issn, (n, pTest)) =>
+      val pRef = freqHistoRef (issn)._2
+      val score = computeScore (
+        pReference = pRef,
+        nReference = nRef,
+        pTest = pTest,
+        nTest = nTest)
+      (issn, score)
+     }.toMap
+    
+    new ScopeMatch (
+      targetIssn, scores, 
+      freqHistoRef = freqHisto (referenceHistogram), 
+      freqHistoTest = freqHisto (neighborHistogram))
+  }
+
+  /** First method of Scope Match dimension extraction
+    * It is based on comparing the histograms of journals for given 
+    * subject areas, computed on the manuscripts dataset on one hand and
+    * inside a neighborhood of the submitted paper on the other hand. 
+    *
+    */
+  def method1ForEid_0 (subEid: String, targetIssn: String, neighborhoodSize: Int) = {
+    /* gather in a map the nb of occurrences of each issns in
+     * the argument dataset */ 
+    def histo (data: Dataset[_]) = data.
+      groupBy ("issn").
+      count.
+      as [(String, Long)].
+      collect.
+      toMap
+    /* converts an count histogram to a frequency histogram */ 
+    def freqHisto (h: Map[String, Long]): (Long, Map[String, (Long, Double)]) = {
+      val N = h.foldLeft (0L) { case (a, (_, n)) => a + n }
+      (N, h.mapValues { n => (n, n.toDouble / N) })
+    }
+
+    /* find all the papers that share at least one subject area 
+     * with the targeted issn. They are the "slibings" */
     val targetSubjArea : Seq[String]= subjareasMap (targetIssn).toSeq
     val slibingsManuscripts = manuscripts.
       filter { ! _.subjareas.intersect (targetSubjArea).isEmpty }.
@@ -145,10 +210,20 @@ class ScopeMatchApp (val embeddingApp : EmbeddingApp) {
         nReference = nRef,
         pTest = pTest,
         nTest = nTest)
-      (issn, (n, pTest, score))
+      (issn, score)
      }.toMap
     
     (scores, freqHisto (referenceHistogram), freqHisto (neighborHistogram))
+  }
+
+  def method1ForEid (subEid: String, targetIssn: String, neighborhoodSize: Int) = {
+    val subEmbedding = manuscriptsEmbeddings.filter ($"id" === subEid).head.embedding
+    method1ForEmbedding (subEmbedding, targetIssn, neighborhoodSize, subEid)
+  }
+
+  def method1ForText (text: String, targetIssn: String, neighborhoodSize: Int) = {
+    val subEmbedding = computeTextEmbedding (Seq (text))(0)
+    method1ForEmbedding (subEmbedding, targetIssn, neighborhoodSize)
   }
 }
 
